07b30d957d41956878257db6bb4bfcc0df476794
JaapK <s1138474@student.windesheim.nl>
2019-11-21 13:59:47 +0100

    login

--- login.php
+++ login.php
@@ -17,14 +17,16 @@
 </head>
 
 <body>
-<h2>Inloggen</h2><br><br>
+<div class="login">
+<h2>Inloggen</h2><br>
     <form>
-    <input type="text" name="Naam"><br>
-    <input type="password" name="passwd"><br>
+    <input type="text" name="Naam"><br><br>
+    <input type="password" name="passwd"><br><br>
     <input type="submit" name="verzenden"><br><br>
-    <h3>Nog geen account? Maak dan nu een account aan.</h3><br>
-        <a href="account.php"><h4>Account aanmaken</h4></a>
 </form>
+</div>
+<div class="login2"><h3>Nog geen account? Maak dan nu een account aan.</h3>
+    <a href="account.php"><h4>Account aanmaken</h4></a></div>
 </body>
 <footer class="footer">
     <h3>© Copyrights 2019 - World Wide Importers</h3>
