b58a8101b45badcd471c11d6f19e2509f20858a3
Jaap <s1138474@student.windesheim.nl>
2019-11-27 21:19:21 +0100

    fix

--- PHP/connectdb.php
+++ PHP/connectdb.php
@@ -4,7 +4,7 @@ mysqli_report(MYSQLI_REPORT_STRICT); //initieer reporting
 $host = "localhost";
 $databasename = "wideworldimporters";
 $user = "root";
-$pass = "ISc00l221$"; //eigen password invullen
+$pass = ""; //eigen password invullen
 $port = 3306;
 
 // Proberen connectie maken, als het niet lukt geef error
