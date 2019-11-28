3f708b2215737e29be959086424a6f2ef562b020
JaapK <s1138474@student.windesheim.nl>
2019-11-28 10:16:59 +0100

    opmaak

--- header.php
+++ header.php
@@ -16,14 +16,9 @@ if (strpos($_SERVER['SCRIPT_NAME'], 'search.php') === false) {
         <a href="index.php"><img src="IMG/wwi-logo.png"></a>
     </div>
     <div class="topnav">
-<<<<<<< HEAD
-
-        <a href="accaanmaken.php"><h3>Account aanmaken</h3></a>
-=======
->>>>>>> b58a8101b45badcd471c11d6f19e2509f20858a3
         <a href="cart.php"><img src="IMG/winkelmand.png" width="65" height="59.5"></a>
-        <a href="account.php"><h3>Account aanmaken</h3></a>
-        <a href="login.php"><h3>Login</h3></a>
+        <a href="login.php"><h3>Log in</h3></a>
+        <a href="accaanmaken.php"><h3>Account aanmaken</h3></a>
         <a href="contact.php"><h3>Contact</h3></a>
         <form class="nav-search" method="get" action="search.php">
             <input class="text" type="text" name="searchinput">
--- index.php
+++ index.php
@@ -5,12 +5,7 @@ include("PHP/functions.php");
 
 $uri = getURI(); // Get uri of page
 
-//if () {
-//include ("header2.php");
-//}
-//else {
 include ("header.php");
-//}
 ?>
 
 <body>
