7f5e0f4c9e118d7c0fb331244a13804f29f00e55
unclefrans <franstuinstra@hotmail.com>
2019-12-03 17:32:05 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -4,13 +4,8 @@ include ("PHP/connectdb.php");
 include ("PHP/functions.php");
 include ("header.php");
 
-?>
-
-<body>
-<h1>Mandje</h1>
-<?php
 //$connect = mysqli_connect("localhost", "root", "", "wideworldimporters");
-foreach($_SESSION["shopping_cart"] as $keys => $values) {
+foreach ($_SESSION["shopping_cart"] as $keys => $values) {
     if (isset($_GET["id"])) {
         if ($_GET["id"] == $_SESSION['shopping_cart'][$keys]['item_id']) {
             if (isset($_POST["plus"])) {
@@ -19,27 +14,27 @@ foreach($_SESSION["shopping_cart"] as $keys => $values) {
             } elseif (isset($_POST["min"])) {
                 $_SESSION['shopping_cart'][$keys]['item_quantity']--;
 
-            } 
+            }
         }
     }
 }
 
-foreach($_SESSION["shopping_cart"] as $keys => $values) {
+foreach ($_SESSION["shopping_cart"] as $keys => $values) {
     if ($values["item_quantity"] <= 0) {
         unset($_SESSION["shopping_cart"][$keys]);
     }
 }
 
-if(isset($_POST["add_to_cart"])){
-    if(isset($_SESSION["shopping_cart"]))    {
+if (isset($_POST["add_to_cart"])) {
+    if (isset($_SESSION["shopping_cart"])) {
         $item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
-        if(!in_array($_GET["id"], $item_array_id))        {
+        if (!in_array($_GET["id"], $item_array_id)) {
             $count = count($_SESSION["shopping_cart"]);
             $item_array = array(
-                'item_id'			=>	$_GET["id"],
-                'item_name'			=>	$_POST["hidden_name"],
-                'item_price'		=>	$_POST["hidden_price"],
-                'item_quantity'		=>	$_POST["quantity"]
+                'item_id' => $_GET["id"],
+                'item_name' => $_POST["hidden_name"],
+                'item_price' => $_POST["hidden_price"],
+                'item_quantity' => $_POST["quantity"]
             );
             $_SESSION["shopping_cart"][$count] = $item_array;
         } else {
@@ -47,19 +42,19 @@ if(isset($_POST["add_to_cart"])){
         }
     } else {
         $item_array = array(
-            'item_id'			=>	$_GET["id"],
-            'item_name'			=>	$_POST["hidden_name"],
-            'item_price'		=>	$_POST["hidden_price"],
-            'item_quantity'		=>	$_POST["quantity"]
+            'item_id' => $_GET["id"],
+            'item_name' => $_POST["hidden_name"],
+            'item_price' => $_POST["hidden_price"],
+            'item_quantity' => $_POST["quantity"]
         );
         $_SESSION["shopping_cart"][0] = $item_array;
     }
 }
 
-if(isset($_GET["action"])) {
-    if($_GET["action"] == "delete")     {
-        foreach($_SESSION["shopping_cart"] as $keys => $values) {
-            if($values["item_id"] == $_GET["id"]) {
+if (isset($_GET["action"])) {
+    if ($_GET["action"] == "delete") {
+        foreach ($_SESSION["shopping_cart"] as $keys => $values) {
+            if ($values["item_id"] == $_GET["id"]) {
                 unset($_SESSION["shopping_cart"][$keys]);
                 echo '<script>alert("Item Removed")</script>';
                 echo '<script>window.location="cart.php"</script>';
@@ -68,6 +63,10 @@ if(isset($_GET["action"])) {
     }
 }
 ?>
+
+<body>
+<h1>Mandje</h1>
+
 <!DOCTYPE html>
 <html>
 <head>
