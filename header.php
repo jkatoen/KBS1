feb52d883dfdb7eff39570187670ec6baa310fd7
JaapK <s1138474@student.windesheim.nl>
2019-12-12 11:17:58 +0100

    opmaak

--- header.php
+++ header.php
@@ -14,28 +14,32 @@ if (strpos($_SERVER['SCRIPT_NAME'], 'search.php') === false) {
     <link rel="stylesheet" type="text/css" href="CSS/mystyle.css">
     <script src="JS/myscript.js"></script>
     <div class="header">
-        <a href="index.php"><img src="IMG/wwi-logo.png"></a>
-    </div>
-    <div class="topnav">
-        <a href="cart.php"><img style="margin-top: 3px" src="IMG/winkelmand.png" width="55" height="49.5"></a>
-        <a href="contact.php"><h4>Contact</h4></a>
-        <?php
-        if (isset($_SESSION["ingelogd"])) {
-            ?>
-            <a href="logout.php"><h4>Log uit</h4></a>
+        <div class="head-left">
+            <a href="index.php"><img src="IMG/wwi-logo.png" class="logo"></a>
+        </div>
+        <div class="head-mid">
+            <form class="nav-search" method="get" action="search.php">
+                <input class="text" type="text" name="searchinput">
+                <input type="submit" name="submitinput" value="Search">
+            </form>
+        </div>
+        <div class="head-right">
+            <a href="cart.php"><img style="margin-top: 3px" src="IMG/winkelmand.png" width="55" height="49.5"></a>
+            <a href="contact.php"><h4>Contact</h4></a>
             <?php
-        }
-        else {
+            if (isset($_SESSION["ingelogd"])) {
+                ?>
+                <a href="logout.php"><h4>Log uit</h4></a>
+                <?php
+            }
+            else {
+                ?>
+                <a href="login.php"><h4>Log in</h4></a>
+                <a href="accaanmaken.php"><h4>Account aanmaken</h4></a>
+                <?php
+            }
             ?>
-        <a href="login.php"><h4>Log in</h4></a>
-        <a href="accaanmaken.php"><h4>Account aanmaken</h4></a>
-        <?php
-        }
-        ?>
-        <form class="nav-search" method="get" action="search.php">
-            <input class="text" type="text" name="searchinput">
-            <input type="submit" name="submitinput" value="Search">
-        </form>
+        </div>
     </div>
 </head>
 </html>
--- login.php
+++ login.php
@@ -9,7 +9,7 @@ include("header.php");
 <div class="row">
 
     <div class="leftcolumn">
-        <div class="card">
+        <div class="card-left">
             <h2>Category</h2>
             <div class="category-container">
                 <?php displayLeftCategories($connection); ?>
