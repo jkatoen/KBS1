1d4c13ca2a96f27e243e435daff4b056a4f7aa6d
Julien <julianvanrijckevorsel@gmail.com>
2019-12-12 14:46:24 +0100

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -38,18 +38,20 @@ $total = $_SESSION["total"];
                     }
                     ?>
                 </form>
-                </div>
-    <div class="Discount">
-        <form method="POST">
-            <input type="text" value="Voer hier je coupon in" name="discount" class="text">
-            <input type="submit" value="Voeg toe" name="ok" required/>
-        </form>
-    </div>
-    <?php
-    if(isset($_POST["discount"])){
+        <div class="Discount">
+            <form method="POST">
+                <p>Voeg hier je coupon toe!</p>
+                <input type="text" name="discount" class="text">
+                <input type="submit" value="Voeg toe" name="ok" required/>
+            </form>
+        </div>
+        <?php
+        if(isset($_POST["discount"])){
 
-    }
-    ?>
+        }
+        ?>
+        </div>
+
     <div class="rightcolumn">
                     <p>Items in je winkelmand</p>
                     <?php
