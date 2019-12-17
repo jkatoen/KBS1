87a2a8bf5a95d6d1b99a443cbc8448c7f6cc4d07
JaapK <s1138474@student.windesheim.nl>
2019-12-17 12:14:08 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -15,7 +15,7 @@ include("header.php");
             <div class="inlog" class="inlog">
                 <h1>Contact opnemen</h1>
                     <form action="mail.php"" method="POST" id="contact">
-                        E-mail: <br><input type="text" name="to" class="textinbox" value="<?php ?>" required/><br><br>
+                        E-mail: <br><input type="text" name="to" class="textinbox" value="" required/><br><br>
                        Onderwerp: <br><input type="text" name="subject" class="textinbox" value="" required/><br><br>
                         Bericht: <br><textarea rows="8" cols="50" form="contact" name="message" class="textinbox" required></textarea><br><br>
                         <input type="submit" value="Versturen" name="send" class="button" required/>
