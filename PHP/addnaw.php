59a088be141823197f177609bb1adf1161dc092a
BasHendriks <bashendriks10@gmail.com>
2019-12-17 11:29:36 +0100

    fix form cart

---
+++ PHP/addnaw.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+print_r($_POST);
+
+$_SESSION["lastname"] = $_POST["lastname"];
+$_SESSION["firstname"] = $_POST["firstname"];
+$_SESSION["address"] = $_POST["address"];
+$_SESSION["email"] = $_POST["email"];
